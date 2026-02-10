f6429939c530eb6c9fd72346419174e0565a8c01
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 09:41:08 +0900

    fxmanifestを整形

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
-author 'Null'
-description 'Notify勉強用スクリプト'
+author 'senganull'
 version '1.0.0'
-
-lua54 'yes' 
+ 
 
 shared_scripts {
     '@ox_lib/init.lua'
